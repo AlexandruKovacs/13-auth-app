fec43e65f60ccd72084ed7e42ac6ae3de468a6cd
Alex Kovacs <axkovacs27@gmail.com>
2023-08-26 21:08:22 +0200

    commit

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject, signal } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { environment } from 'src/environments/environments';
+
+import { User } from '../interfaces';
+import { AuthStatus } from '../interfaces/auth-status.enum';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private readonly baseUrl: string = environment.baseUrl;
+  private http = inject( HttpClient );
+
+  private _currentUser = signal<User | null>( null );
+  private _authStatus = signal<AuthStatus>( AuthStatus.checking );
+
   constructor() { }
+
+  login( email: string, password: string ): Observable<boolean> {
+
+    return of(true);
+  }
+
 }
