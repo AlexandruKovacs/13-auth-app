466678cc666ff33719e4b92ef00a3f7c1ab6aeea
Alex Kovacs <axkovacs27@gmail.com>
2023-08-26 21:26:33 +0200

    commit

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   templateUrl: './login-page.component.html',
@@ -8,6 +9,7 @@ import { FormBuilder, Validators } from '@angular/forms';
 export class LoginPageComponent {
 
   private fb = inject( FormBuilder );
+  private authService = inject( AuthService );
 
   public loginForm = this.fb.group({
     email: ['', [ Validators.required, Validators.email ]],
@@ -15,7 +17,16 @@ export class LoginPageComponent {
   });
 
   login() {
-    console.log( this.loginForm.value );
+    const { email, password } = this.loginForm.value;
+
+    if (email !== null && email !== undefined && password !== null && password !== undefined) {
+      this.authService.login(email, password)
+        .subscribe(success => {
+          console.log(success);
+        });
+    } else {
+      console.error("Email and password must have valid values.");
+    }
   }
 
 }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,9 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject, signal } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Injectable, computed, inject, signal } from '@angular/core';
+import { Observable, map, of, tap } from 'rxjs';
 import { environment } from 'src/environments/environments';
 
-import { User } from '../interfaces';
+import { LoginResponse, User } from '../interfaces';
 import { AuthStatus } from '../interfaces/auth-status.enum';
 
 @Injectable({
@@ -14,14 +14,33 @@ export class AuthService {
   private readonly baseUrl: string = environment.baseUrl;
   private http = inject( HttpClient );
 
-  private _currentUser = signal<User | null>( null );
+  private _currentUser = signal<User | null>(null);
   private _authStatus = signal<AuthStatus>( AuthStatus.checking );
+
+  // Al mundo exterior
+  public currentUser = computed( () => this._currentUser() );
+  public authStatus = computed( () => this._authStatus() );
 
   constructor() { }
 
   login( email: string, password: string ): Observable<boolean> {
 
-    return of(true);
+    const url = `${ this.baseUrl }/auth/login`;
+    const body = { email, password };
+
+    return this.http.post<LoginResponse>( url, body )
+      .pipe(
+        tap( ({ user, token }) => {
+          this._currentUser.set( user );
+          this._authStatus.set( AuthStatus.authenticated );
+          localStorage.setItem( 'token', token );
+          console.log({ user, token });
+        }),
+
+        map( () => true )
+
+        // Error
+      );
   }
 
 }
