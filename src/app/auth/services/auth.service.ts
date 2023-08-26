f0d94134fd3f1c6c2c7a12f40c3f07ef79e14f53
Alex Kovacs <axkovacs27@gmail.com>
2023-08-26 22:04:29 +0200

    commit

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, computed, inject, signal } from '@angular/core';
 import { Observable, catchError, map, of, tap, throwError } from 'rxjs';
 import { environment } from 'src/environments/environments';
 
-import { LoginResponse, User } from '../interfaces';
+import { CheckTokenResponse, LoginResponse, User } from '../interfaces';
 import { AuthStatus } from '../interfaces/auth-status.enum';
 
 @Injectable({
@@ -36,12 +36,37 @@ export class AuthService {
           localStorage.setItem( 'token', token );
           console.log({ user, token });
         }),
-
         map( () => true ),
-
-        // Error
         catchError( err => throwError( () => err.error.message ))
       );
+  }
+
+  checkAuthStatus(): Observable<boolean> {
+    const url = `${ this.baseUrl }/auth/check-token`;
+    const token = localStorage.getItem('token');
+
+    if (!token) {
+      this._authStatus.set( AuthStatus.notAuthenticated );
+      return of(false);
+    }
+
+    const headers = new HttpHeaders()
+      .set('Authorization', `Bearer ${ token }`);
+
+    return this.http.get<CheckTokenResponse>( url, { headers } )
+      .pipe(
+        map( ({ token, user }) => {
+          this._currentUser.set( user );
+          this._authStatus.set( AuthStatus.authenticated );
+          localStorage.setItem( 'token', token );
+
+          return true;
+        }),
+        catchError( () => {
+          this._authStatus.set( AuthStatus.notAuthenticated );
+          return of(false);
+        })
+      )
   }
 
 }
